082f162f9336518448f38709eb640983894fc658
Imaven1 <158128049+Imaven1@users.noreply.github.com>
2025-07-01 20:37:49 +0800

    Implementation for DepartmentController + HospitalController w/ Hospital view files

---
+++ DMS/resources/js/Pages/Hospitals/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+    <AppLayout>
+        <div class="custom-title mb-4">Edit Hospital</div>
+    
+        <v-form @submit.prevent="submit">
+        <v-text-field v-model="form.name" label="Hospital Name" required></v-text-field>
+        <v-text-field v-model="form.branch" label="Branch" required></v-text-field>
+    
+        <v-btn type="submit" color="primary">Edit Hospital</v-btn>
+        </v-form>
+    </AppLayout>
+</template>
+
+<script>
+
+</script>
+
