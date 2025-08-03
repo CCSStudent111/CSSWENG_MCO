247959859008af747b5a84f833932bd5cd5888a1
swifted03 <franzcolobong23@gmail.com>
2025-08-03 14:03:25 +0800

    Fix login

--- DMS/resources/js/Pages/Auth/Login.vue
+++ DMS/resources/js/Pages/Auth/Login.vue
@@ -1,26 +1,19 @@
 <template>
     <v-container class="center-height d-flex" fluid>
-        <v-row align="center"
-               justify="center">
+        <v-row align="center" justify="center">
             <v-spacer></v-spacer>
             <v-col class="d-flex justify-center">
                 <v-sheet elevation="5" height="370" width="400" rounded="lg">
                     <div class="in-title mt-5">Sign In</div>
                     <form @submit.prevent="form.post(route('login.submit'))" class="pa-2">
-                        <v-text-field hide-details
-                                      label="Username"
-                                      name="username"
-                                      variant="outlined"
-                                      class="in-input"
-                                      required></v-text-field>
-                        <v-text-field hide-details
-                                      label="Password"
-                                      name="password"
-                                      type="password"
-                                      variant="outlined"
-                                      class="in-input"
-                                      required></v-text-field>
-                        <v-btn class="mt-3 in-button" color="black" type="submit" :loading="form.processing" block rounded>
+                        <v-text-field v-model="form.username" hide-details="auto" label="Username" name="username"
+                            variant="outlined" class="in-input" :error-messages="form.errors.username" required />
+                        <v-text-field v-model="form.password" hide-details="auto" label="Password" name="password"
+                            type="password" variant="outlined" class="in-input" :error-messages="form.errors.password"
+                            required />
+
+                        <v-btn class="mt-3 in-button" color="black" type="submit" :loading="form.processing" block
+                            rounded>
                             Log In
                         </v-btn>
                     </form>
@@ -39,5 +32,4 @@ const form = useForm({
 })
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
